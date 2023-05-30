0a4da4508bf7c75a6d14125e8fdbf3894164e0ff
이수영 <suyoung@isuyeong-ui-MacBookAir.local>
2023-05-30 17:17:36 +0900

    upload sql

---
+++ sql/ddl_raw_data/api_repos_issues_labels.sql
@@ -0,0 +1,10 @@
+CREATE TABLE adhoc.api_repos_issuses_labels (
+  id SERIAL PRIMARY KEY,
+  issues_id INT NOT NULL,
+  labels_id INT NOT NULL,
+  labels_url TEXT,
+  name VARCHAR(255),
+  color VARCHAR(255),
+  description TEXT,
+  repo_full_name VARCHAR(255) NOT NULL
+);
