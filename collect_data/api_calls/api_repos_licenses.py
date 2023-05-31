c3cf28c6368407f29ddb960ae8a28a95168c35f9
An Nyeong <me@annyeong.me>
2023-05-31 15:03:34 +0900

    fix: add common.py to share common functions

--- collect_data/api_calls/api_repos_licenses.py
+++ collect_data/api_calls/api_repos_licenses.py
@@ -1,34 +1,5 @@
-import requests
 from typing import List, Dict
-
-def get_json(uri: str, headers: Dict) -> Dict:
-    """
-    uri를 기반으로 GitHub API를 보내고 JSON 응답을 dict로 반환합니다.
-
-    Args:
-        uri (str): 요청을 보내려는 URI
-        headers (dict): HTTP 요청 헤더. 'Authorization'가 필요합니다.
-    
-    Returns:
-        dict: 요청에 대한 JSON 응답
-    
-    Rasises:
-        ValueError: API 실패 시
-    
-    Example:
-        headers={
-            'Accept': 'application/vnd.github+json',
-            'Authorization': 'Bearer ' + <github_access_token>,
-            'X-GitHub-Api-Version': '2022-11-28',
-        }
-        data = get_json('/repos/owner/repo', headers)
-    """
-    url = f'https://api.github.com{uri}'
-    req = requests.get(url, headers=headers)
-    if req.status_code != 200: # TODO: status_code에 맞게 Error 사용ㅓ
-        raise ValueError
-    else:
-        return req.json()
+from collect_data.common import github_api
 
 def create_repo_license_dict(json: Dict, repo_full_name: str, current_time: str) -> Dict:
     return {
@@ -67,7 +38,7 @@ def collect_api_repos_licenses(headers: Dict, repos: List[str], current_time: st
     """
     data = []
     for repo in repos:
-        json = get_json(f'/repos/{repo}/license', headers)
+        json = github_api(f'/repos/{repo}/license', headers)
         license_dict = create_repo_license_dict(json, repo, current_time)
         data.append(license_dict)
     return data
