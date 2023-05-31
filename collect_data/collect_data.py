68d20d65c1837a8b7eebd3ff21fc31485b394a32
minseok.kim <kmus1232@gmail.com>
2023-05-31 16:49:45 +0900

    refactor: Gets the current time immediately from the datetime module

--- collect_data/collect_data.py
+++ collect_data/collect_data.py
@@ -16,20 +16,10 @@ ORGS = ['moloco', 'woowabros', 'daangn', 'toss',
         'ncsoft', 'line', 'kakao', 'naver', 'nhn']
 
 
-def get_current_time():
-    """
-    현재 시간과 날짜를 문자열 형태로 반환하는 함수입니다.
-
-    Returns:
-        str -> 2023-05-30 18:42:24
-    """
-    current_datetime = datetime.datetime.now()
-    return current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-
 
 def run():
     db = psqlConnector()
-    CURRENT_TIME = get_current_time()
+    CURRENT_TIME = datetime.datetime.now()
 
     orgs_data = collect_api_orgs(HEADERS, ORGS, CURRENT_TIME)
     for values in orgs_data:
